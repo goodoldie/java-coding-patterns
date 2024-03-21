5aad7e0cbef0465431c6bf74361ea24ee66e7087
goodoldie <goodoldie1996@gmail.com>
2024-03-21 12:37:44 +0530

    Added Reverse K Groups in Linked List problem.

--- Java Coding Patterns/Basic Data Structures/In Place Reversa Of LinkedList/src/ReverseKGroup.java
+++ Java Coding Patterns/Basic Data Structures/In Place Reversa Of LinkedList/src/ReverseKGroup.java
@@ -49,11 +49,69 @@ public class ReverseKGroup {
                 System.out.println(e.getMessage());
             }
         }
+
+        public LinkedListNode[] reverseLinkedList(LinkedListNode node, int k){
+            LinkedListNode previous = null;
+            LinkedListNode current = node;
+            LinkedListNode next = null;
+
+            for (int i = 0; i < k; i++) {
+                next = current.next;
+                current.next = previous;
+                previous = current;
+                current = next;
+            }
+            return new LinkedListNode[]{previous, current};
+        }
+
+        public LinkedListNode reverseKGroupNodes(LinkedListNode head, int k) throws NullPointerException{
+            if(head == null)
+                throw new NullPointerException("Linked List cannot be empty");
+
+            LinkedListNode dummyNode = new LinkedListNode(0);
+            dummyNode.next = head;
+            LinkedListNode pointer = dummyNode;
+
+            while(pointer != null){
+                LinkedListNode tracker = pointer;
+                for (int i = 0; i < k; i++) {
+                    // if at anypoint in a group, the tracker becomes null, we exit
+                    if (tracker == null) {
+                        break;
+                    }
+                    tracker = tracker.next;
+                }
+                //if after traversing a group the tracker is pointing to null, it means we have reached the end of list
+                if(tracker == null)
+                    break;
+
+                LinkedListNode[] updatedNodes = reverseLinkedList(pointer.next, k);
+                LinkedListNode previous = updatedNodes[0];
+                LinkedListNode current = updatedNodes[1];
+                LinkedListNode lastNodeOfReversedGroup = pointer.next;
+                lastNodeOfReversedGroup.next = current;
+                pointer.next = previous;
+                pointer = lastNodeOfReversedGroup;
+            }
+            return dummyNode;
+        }
     }
 
 
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int[] inputArray = {1,2,3,4,5,6,7};
+        int k = 3;
+        try{
+            LinkedList linkedList = new LinkedList();
+            linkedList.createLinkedList(inputArray);
+            linkedList.printLinkedList(linkedList.head);
+            System.out.println("Linked list after reversing in groups of " +  k + " is");
+            linkedList.head = linkedList.reverseKGroupNodes(linkedList.head, k);
+            linkedList.printLinkedList(linkedList.head.next);
+        } catch (Exception e){
+            System.out.println("Exception: " + e.getMessage());
+        }
     }
 }
